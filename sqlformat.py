06f68fac3f2fccc4b33102a98bf440da3caedad1
zengfan1992 <115183414+zengfan1992@users.noreply.github.com>
2024-05-05 11:32:50 -0600

    Add files via upload

--- sqlformat.py
+++ sqlformat.py
@@ -3,4 +3,14 @@ import sqlparse
 import io
 
 with open(sys.argv[1], "r", encoding="utf-8") as f:
-    print(sqlparse.format(f.read(), reindent=True), end="")
+    # print(";\n".join(map(lambda x: sqlparse.format(x, reindent=True),sqlparse.split(f.read()))), end="")
+    list = sqlparse.split(f.read())
+    res = []
+    for x in list:
+        if "create table" in x:
+            res.append(sqlparse.format(x))
+            continue
+        s = sqlparse.format(x, reindent=True)
+        if len(s) > 1:
+            res.append(s.replace("\n\n", "\n"))
+    print("\n\n".join(res), end="")
